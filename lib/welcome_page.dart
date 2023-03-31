da4c1512dc48ce7c3acace949c21ae170998bb54
Nur Sakinah <nursakinah2402@gmail.com>
2023-03-31 23:41:49 +0700

    adding assets and button on welcome_page

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -18,15 +18,15 @@ class _WelcomePageState extends State<WelcomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Container(
-              margin: EdgeInsets.only(top: 80),
-              alignment: Alignment.topCenter,
-              child: RichText(
-                text: TextSpan(
-                    text: 'Pokedex.',
-                    style: const TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(236, 243, 192, 26))))),
+                margin: EdgeInsets.only(top: 80),
+                alignment: Alignment.topCenter,
+                child: RichText(
+                    text: TextSpan(
+                        text: 'Pokedex.',
+                        style: const TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(236, 243, 192, 26))))),
             Container(
               margin: EdgeInsets.only(top: 30),
               alignment: Alignment.topCenter,
@@ -36,12 +36,26 @@ class _WelcomePageState extends State<WelcomePage> {
                 style: TextStyle(color: Colors.grey),
               ),
             ),
-             Container(
-              margin: EdgeInsets.only(top: 100),
+            Container(
+              margin: EdgeInsets.only(top: 90),
               alignment: Alignment.topCenter,
-              child: Image.network('https://cutewallpaper.org/24/pokeballs-png/pokeball-png-photo-image-png-play.png'),
-              width: 200, height: 200,
+              child: Image.network(
+                  'https://cutewallpaper.org/24/pokeballs-png/pokeball-png-photo-image-png-play.png'),
+              width: 200,
+              height: 200,
+            ),
+            Container(
+              height: 70,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Icon(Icons.arrow_right_alt_sharp),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(236, 243, 192, 26),
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(24),
               ),
+            )
           ],
         ),
       ),
