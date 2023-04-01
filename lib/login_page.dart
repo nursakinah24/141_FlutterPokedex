ffa9764a15f38304dc105be8fdd270b163087a71
Nur Sakinah <nursakinah2402@gmail.com>
2023-04-01 23:57:00 +0700

    update login_page and added widgets on register_page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_pokedex/home_page.dart';
+import 'package:flutter_pokedex/register_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -20,151 +21,191 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      key: _formkey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 30),
-            child: Image.network(
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png',
-                width: 400,
-                height: 200),
-          ),
-          Container(
-              // margin: EdgeInsets.only(top: 20),
-              //alignment: Alignment.topCenter,
-              child: RichText(
-                  text: const TextSpan(
-                      text: "Let's Get Started",
-                      style: const TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(236, 243, 192, 26))))),
-          Container(
-            margin: EdgeInsets.only(top: 10),
-            alignment: Alignment.topCenter,
-            child: const Text(
-              'There is No Sense In Going Out of \n Your Way to Get Somebody to Like You.',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 30, left: 18),
-            alignment: Alignment.topLeft,
-            child: const Text(
-              'Name',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontSize: 17,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            //margin: EdgeInsets.only(left: ),
-            /*  child: ConstrainedBox(
-                    constraints: BoxConstraints.tight(const Size(380, 200)), */
-            padding: EdgeInsets.only(left: 15, right: 20),
-            child: TextFormField(
-              decoration: const InputDecoration(
-                  // labelText: 'Name',
-                  hintText: 'Enter your name',
-                  //border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.all(20.0),
-                  prefixIcon: Icon(Icons.person, size: 30, color: Colors.blue),
-                  enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue))),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Nama Tidak Boleh Kosong';
-                }
-                if (!value!.contains('Kina')) {
-                  return 'Please enter a valid name!';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                name = value;
-              },
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 25, left: 18),
-            alignment: Alignment.topLeft,
-            child: const Text(
-              'Password',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontSize: 17,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 15, right: 20, bottom: 50),
-            child: TextFormField(
-              obscureText: _obscureText,
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                //border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.all(20.0),
-                enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue)),
-                prefixIcon: Icon(Icons.lock, size: 30, color: Colors.blue),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscureText ? Icons.visibility : Icons.visibility_off,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _obscureText = !_obscureText;
-                    });
-                  },
-                ),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your password';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                password = value;
-              },
-            ),
-          ),
-          ConstrainedBox(
-              constraints:
-                  const BoxConstraints.tightFor(width: 300, height: 50),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(236, 243, 192, 26),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)),
-                  //padding: EdgeInsets.all(24),
-                ),
-                onPressed: () {
-                  if (_formkey.currentState!.validate()) {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => HomePage(
-                          name: name,
-                          password: password,
+            child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 30),
+                        child: Image.network(
+                            'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png',
+                            width: 400,
+                            height: 200),
+                      ),
+                      Container(
+                          // margin: EdgeInsets.only(top: 20),
+                          //alignment: Alignment.topCenter,
+                          child: RichText(
+                              text: const TextSpan(
+                                  text: "Let's Get Started",
+                                  style: const TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold,
+                                      color:
+                                          Color.fromARGB(236, 243, 192, 26))))),
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        alignment: Alignment.topCenter,
+                        child: const Text(
+                          'There is No Sense In Going Out of \n Your Way to Get Somebody to Like You.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.grey, fontWeight: FontWeight.bold),
                         ),
                       ),
-                    );
-                    setState(() {});
-                  }
-                },
-                child: const Text(
-                  'Sign In',
-                ),
-              ))
-        ],
-      ),
-    ));
+                      Container(
+                        margin: EdgeInsets.only(top: 30, left: 18),
+                        alignment: Alignment.topLeft,
+                        child: const Text(
+                          'Name',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        //margin: EdgeInsets.only(left: ),
+                        /*  child: ConstrainedBox(
+                    constraints: BoxConstraints.tight(const Size(380, 200)), */
+                        padding: EdgeInsets.only(left: 15, right: 20),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              // labelText: 'Name',
+                              hintText: 'Enter your name',
+                              //border: OutlineInputBorder(),
+                              contentPadding: EdgeInsets.all(20.0),
+                              prefixIcon: Icon(Icons.person,
+                                  size: 30, color: Colors.blue),
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.blue))),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Nama Tidak Boleh Kosong';
+                            }
+                            if (!value!.contains('Kina')) {
+                              return 'Please enter a valid name!';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            name = value;
+                          },
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 25, left: 18),
+                        alignment: Alignment.topLeft,
+                        child: const Text(
+                          'Password',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        padding:
+                            EdgeInsets.only(left: 15, right: 20, bottom: 45),
+                        child: TextFormField(
+                          obscureText: _obscureText,
+                          decoration: InputDecoration(
+                            hintText: 'Enter your password',
+                            //border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.all(20.0),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.blue)),
+                            prefixIcon:
+                                Icon(Icons.lock, size: 30, color: Colors.blue),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscureText
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                });
+                              },
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            password = value;
+                          },
+                        ),
+                      ),
+                      ConstrainedBox(
+                          constraints: const BoxConstraints.tightFor(
+                              width: 370, height: 50),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Color.fromARGB(236, 243, 192, 26),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50)),
+                              //padding: EdgeInsets.all(24),
+                            ),
+                            onPressed: () {
+                              if (_formkey.currentState!.validate()) {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => HomePage(
+                                      name: name,
+                                      password: password,
+                                    ),
+                                  ),
+                                );
+                                setState(() {});
+                              }
+                            },
+                            child: const Text(
+                              'Sign In',
+                            ),
+                          )),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Don't have an account?",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Container(
+                              width: 5,
+                            ),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => RegisterPage(),
+                                    ),
+                                  );
+                                },
+                                child: const Text(
+                                  "Sign Up",
+                                  style: TextStyle(
+                                      color: Color.fromARGB(236, 243, 192, 26),
+                                      fontWeight: FontWeight.bold),
+                                )),
+                          ]),
+                    ],
+                  ),
+                ))));
   }
 }
---
+++ lib/register_page.dart
@@ -0,0 +1,301 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/home_page.dart';
+import 'package:flutter_pokedex/login_page.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? name = '';
+  String? email = '';
+  String? password = '';
+  String? confirmPassword = '';
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 30, left: 10),
+                alignment: Alignment.topLeft,
+                child: ConstrainedBox(
+                  constraints:
+                      const BoxConstraints.tightFor(width: 40, height: 40),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.arrow_back,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(236, 243, 192, 26),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      padding: EdgeInsets.all(24),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 5),
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+                child: Image.network(
+                  'https://www.pngall.com/wp-content/uploads/13/Pokemon-Logo-PNG-Pic.png',
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: 15),
+                  alignment: Alignment.topCenter,
+                  child: RichText(
+                      text: const TextSpan(
+                          text: 'Register',
+                          style: const TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(236, 243, 192, 26))))),
+              Container(
+                margin: EdgeInsets.only(top: 20, left: 18),
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  'Name',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 15, right: 20),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'Enter your name',
+                      contentPadding: EdgeInsets.all(15.0),
+                      prefixIcon:
+                          Icon(Icons.person, size: 30, color: Colors.blue),
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue))),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nama Tidak Boleh Kosong';
+                    }
+                    if (!value!.contains(value)) {
+                      return 'Please enter a valid name!';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    name = value;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10, left: 18),
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  'Email',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 15, right: 20),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'Enter your email',
+                      contentPadding: EdgeInsets.all(15.0),
+                      prefixIcon:
+                          Icon(Icons.email, size: 30, color: Colors.blue),
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue))),
+                  validator: (value) {
+                    if (!value!.contains('@')) {
+                      return 'Format Email Salah';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10, left: 18),
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  'Password',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 15, right: 20),
+                child: TextFormField(
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your password',
+                    //border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.all(15.0),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)),
+                    prefixIcon: Icon(Icons.lock, size: 30, color: Colors.blue),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureText ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    password = value;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10, left: 18),
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  'Re-Password',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 15, right: 20, bottom: 20),
+                child: TextFormField(
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your password',
+                    //border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.all(15.0),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)),
+                    prefixIcon: Icon(Icons.lock, size: 30, color: Colors.blue),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureText ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please confirm your password';
+                    }
+                    if (value != password) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    confirmPassword = value;
+                  },
+                ),
+              ),
+              ConstrainedBox(
+                  constraints:
+                      const BoxConstraints.tightFor(width: 370, height: 50),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(236, 243, 192, 26),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      //padding: EdgeInsets.all(24),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomePage(
+                              name: name,
+                              password: password,
+                            ),
+                          ),
+                        );
+                        setState(() {});
+                      }
+                    },
+                    child: const Text(
+                      'Register',
+                    ),
+                  )),
+              /* TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Confirm Password',
+                  icon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please confirm your password';
+                  }
+                  if (value != password) {
+                    return 'Passwords do not match';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  confirmPassword = value;
+                },
+              ), */
+              /*  ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    // TODO: Do register process here
+                  }
+                },
+                child: Text('Register'),
+              ), */
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  String? validateEmail(String? value) {
+    if (!value!.contains('@')) {
+      return 'False format email!';
+    }
+    return null;
+  }
+}
