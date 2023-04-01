3fd10b96f03c8e151054a78668c52f93fcccb0a8
Nur Sakinah <nursakinah2402@gmail.com>
2023-04-01 19:45:00 +0700

    added TextFormField for Password

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,15 +12,17 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _formkey = GlobalKey<FormState>();
   String? name;
-  String? email;
+  String? password;
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
+          key: _formkey,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        children: [
+        children: <Widget> [
           Container(
             margin: EdgeInsets.only(top: 30),
             child: Image.network(
@@ -32,7 +34,7 @@ class _LoginPageState extends State<LoginPage> {
               // margin: EdgeInsets.only(top: 20),
               //alignment: Alignment.topCenter,
               child: RichText(
-                  text: TextSpan(
+                  text: const TextSpan(
                       text: "Let's Get Started",
                       style: const TextStyle(
                           fontSize: 30,
@@ -41,7 +43,7 @@ class _LoginPageState extends State<LoginPage> {
           Container(
             margin: EdgeInsets.only(top: 10),
             alignment: Alignment.topCenter,
-            child: Text(
+            child: const Text(
               'There is No Sense In Going Out of \n Your Way to Get Somebody to Like You.',
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
@@ -50,7 +52,7 @@ class _LoginPageState extends State<LoginPage> {
           Container(
             margin: EdgeInsets.only(top: 30, left: 18),
             alignment: Alignment.topLeft,
-            child: Text(
+            child: const Text(
               'Name',
               textAlign: TextAlign.left,
               style: TextStyle(fontSize: 17, color: Colors.blue, fontWeight: FontWeight.bold),
@@ -75,15 +77,66 @@ class _LoginPageState extends State<LoginPage> {
                 if (value == null || value.isEmpty) {
                   return 'Nama Tidak Boleh Kosong';
                 }
+                if (!isValidName(value)){
+                  return 'Please enter a valid name!';
+                }
                 return null;
               },
               onSaved: (value) {
                 name = value;
               },
             ),
-          )
+          ),
+           Container(
+            margin: EdgeInsets.only(top: 25, left: 18),
+            alignment: Alignment.topLeft,
+            child: const Text(
+              'Password',
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 17, color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+          ),
+            Container(
+            padding: EdgeInsets.only(left: 15, right: 20),
+            child: TextFormField(
+              obscureText: _obscureText,
+              decoration:  InputDecoration(
+                hintText: 'Enter your password',
+                //border: OutlineInputBorder(),
+                contentPadding:
+                    EdgeInsets.all(20.0),
+                       enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.blue)),
+                prefixIcon: Icon(Icons.lock, size: 30, color: Colors.blue),
+                suffixIcon: IconButton(icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off,),
+                onPressed: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+              ),
+              ),
+                 validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  password = value;
+                },
+              ),
+            )
+            
         ],
       ),
-    ));
+    )
+    );
+  }
+
+  bool isValidName(String name){
+    return true;
+  }
+
+  void login(String email, String password) {
   }
 }
